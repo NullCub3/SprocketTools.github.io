4600b3abf77a23982631052e9ca19617313e34a2
NullCube <nullcub3@gmail.com>
2024-10-23 06:34:50 -0700

    Contests folder

--- navbar.js
+++ navbar.js
@@ -1,41 +1,14 @@
+import * as navbar from "/scripts/buildNavBar.js";
 const pageTitle = document.title;
-const pages = {
-  Home: "index.html",
-  Gear_Calculator: "TopGearCalculator.html",
-  Contests: "ContestsList.html",
-  Random_Tank_Picker: "VehicleGenerator.html",
-  Guides: "resources.html",
-  Credits: "credits.html",
-  Get_Trolled: "https://www.youtube.com/watch?v=p7YXXieghto",
-  Decal_Catalog: "Decals/DecalsFeatured.html",
-  RGB_Decal_Maker: "Decals/DecalsRGBmaker.html",
-};
-
-var i = 0;
-var list = '<div class="navbar">\n<img src="assets/SprocketToolsLogo.png">\n';
-for (p in pages) {
-  let pname = p.replace(/_/g, " ");
-  let url = pages[`${p}`];
-  if (new RegExp(`.*${pname}.*`).test(pageTitle)) {
-    list += `<a class="active" href="${pages[`${p}`]}">${pname}</a>\n`;
-  } else {
-    list += `<a href="${url}">${pname}</a>\n`;
-  }
-}
-
-console.log(pageTitle);
-console.log(pages);
-console.log(list);
-
-const header = list + "</div>";
 
 const style =
-  '<link rel="icon" type="image/x-icon" href="assets/SprocketToolsLogo.png">';
+  '<link rel="icon" type="image/x-icon" href="/assets/SprocketToolsLogo.png">';
 
-function add_items() {
-  document.body.innerHTML = header + document.body.innerHTML;
-  document.head.innerHTML = style + document.head.innerHTML;
-}
-
-console.log(document);
-window.addEventListener("load", add_items);
+window.addEventListener(
+  "load",
+  navbar.makePage(
+    document,
+    navbar.buildNav(navbar.globalPages, pageTitle),
+    style,
+  ),
+);
