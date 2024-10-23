d42bc46e912fb6fd2057fe9c08901abb74a5e3fb
NullCube <nullcub3@gmail.com>
2024-10-23 06:34:50 -0700

    Contests folder

---
+++ scripts/buildNavBar.js
@@ -0,0 +1,35 @@
+export const globalPages = {
+  Home: "/index.html",
+  Gear_Calculator: "/TopGearCalculator.html",
+  Contests: "/ContestsList.html",
+  Random_Tank_Picker: "/VehicleGenerator.html",
+  Guides: "/resources.html",
+  Credits: "/credits.html",
+  Get_Trolled: "https://www.youtube.com/watch?v=p7YXXieghto",
+  Decal_Catalog: "/Decals/DecalsFeatured.html",
+  RGB_Decal_Maker: "/Decals/DecalsRGBmaker.html",
+};
+
+export function buildNav(pl, pt = "", c = "navbar") {
+  let list = `
+    <div class="${c}">
+    <img src="/assets/SprocketToolsLogo.png">
+`;
+  for (let p in pl) {
+    let pname = p.replace(/_/g, " ");
+    let url = pl[`${p}`];
+
+    if (new RegExp(`.*${pname}.*`).test(pt)) {
+      list += `<a class="active" href="${pl[`${p}`]}">${pname}</a>\n`;
+    } else {
+      list += `<a href="${url}">${pname}</a>\n`;
+    }
+  }
+  let finalBar = `${list}</div>`;
+  return finalBar;
+}
+
+export function makePage(doc, h = "", s = "") {
+  doc.body.innerHTML = h + doc.body.innerHTML;
+  doc.head.innerHTML = s + doc.head.innerHTML;
+}
